0d180ef57c05442ebfa0e09deae5f784919ccfe3
xannosz <joli.neni@freemail.hu>
2020-12-25 22:29:49 +0100

    Create first version

---
+++ src/main/java/hu/xannosz/minecraft/coordinate/holder/Data.java
@@ -0,0 +1,104 @@
+package hu.xannosz.minecraft.coordinate.holder;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import org.apache.commons.io.FileUtils;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Data {
+
+    private final static Path DATA_PATH = Paths.get("data.json");
+
+    public static Data INSTANCE = new Data();
+
+    @Getter
+    private final Set<Coordinate> coordinates = new HashSet<>();
+
+    @Getter
+    @Setter
+    private String mapPrefix="/map";
+
+    @Getter
+    @Setter
+    private String mainPage="google.com";
+
+    public void addCoordinate(String name, int x, int y, int z, Dimension dimension) {
+        coordinates.add(new Coordinate(name, x, y, z, dimension));
+    }
+
+    private Data() {
+
+    }
+
+    public static void readData() {
+        try {
+            JsonElement dataObject = JsonParser.parseString(FileUtils.readFileToString(DATA_PATH.toFile()));
+            INSTANCE = new Gson().fromJson(dataObject, Data.class);
+        } catch (Exception e) {
+            e.printStackTrace();
+            INSTANCE = new Data();
+        }
+    }
+
+    public static void writeData() {
+        try {
+            DATA_PATH.toFile().createNewFile();
+            FileUtils.writeStringToFile(DATA_PATH.toFile(), new Gson().toJson(INSTANCE));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Getter
+    @AllArgsConstructor
+    public static class Coordinate {
+        private final String name;
+        private final int x;
+        private final int y;
+        private final int z;
+        private final Dimension dimension;
+    }
+
+    public static class CoordinateComparator implements Comparator<Coordinate> {
+        @Override
+        public int compare(Coordinate coord1, Coordinate coord2) {
+            if (coord2.getDimension().getI() > coord1.getDimension().getI()) {
+                return 1;
+            } else if (coord2.getDimension().getI() < coord1.getDimension().getI()) {
+                return -1;
+            } else {
+                if (coord1.getX() > coord2.getX()) {
+                    return 1;
+                } else if (coord1.getX() < coord2.getX()) {
+                    return -1;
+                } else {
+                    return Integer.compare(coord1.getZ(), coord2.getZ());
+                }
+            }
+        }
+    }
+
+    public enum Dimension {
+        OVER_WORD(0,"minecraft%20-%20overworld/day"), NETHER(1,"minecraft%20-%20nether/nether"), END(2,"minecraft%20-%20end/end");
+
+        @Getter
+        private final int i;
+
+        @Getter
+        private final String link;
+
+        Dimension(int i,String link) {
+            this.i = i;
+            this.link = link;
+        }
+    }
+}
---
+++ src/main/java/hu/xannosz/minecraft/coordinate/holder/Holder.java
@@ -0,0 +1,207 @@
+package hu.xannosz.minecraft.coordinate.holder;
+
+import hu.xannosz.microtools.FileResourcesUtils;
+import hu.xannosz.microtools.pack.Douplet;
+import hu.xannosz.veneos.core.*;
+import hu.xannosz.veneos.core.css.CssAttribute;
+import hu.xannosz.veneos.core.css.CssComponent;
+import hu.xannosz.veneos.core.css.HtmlSelector;
+import hu.xannosz.veneos.core.css.Selector;
+import hu.xannosz.veneos.core.html.*;
+import hu.xannosz.veneos.next.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class Holder implements HttpHandler {
+    private static final String NAME = "name";
+    private static final String X = "x";
+    private static final String Y = "y";
+    private static final String Z = "z";
+    private static final String DIMENSION = "dimension";
+    private final Theme theme = new Theme();
+    private final HtmlClass copyButton = new HtmlClass();
+
+    public static void main(String[] args) {
+        Holder holder = new Holder();
+    }
+
+    public Holder() {
+        VeneosServer server = new VeneosServer();
+        server.createServer(8000);
+        server.setHandler(this);
+        FileContainer.addFile("copy.png", FileResourcesUtils.getFileFromResourceAsFile("copy.png"));
+        FileContainer.addFile("map.png", FileResourcesUtils.getFileFromResourceAsFile("map.png"));
+        ThemeHandler.registerTheme(theme);
+
+        CssComponent body = new CssComponent(HtmlSelector.BODY.getSelector());
+        body.addAttribute(CssAttribute.COLOR, "lightgray");
+        body.addAttribute(CssAttribute.BACKGROUND_COLOR, "black");
+        theme.add(body);
+
+        CssComponent input = new CssComponent(HtmlSelector.INPUT.getSelector());
+        input.addAttribute(CssAttribute.COLOR, "lightgray");
+        input.addAttribute(CssAttribute.BACKGROUND_COLOR, "black");
+        theme.add(input);
+
+        CssComponent select = new CssComponent(HtmlSelector.SELECT.getSelector());
+        select.addAttribute(CssAttribute.COLOR, "lightgray");
+        select.addAttribute(CssAttribute.BACKGROUND_COLOR, "black");
+        theme.add(select);
+
+        CssComponent div = new CssComponent(HtmlSelector.DIV.getSelector());
+        div.addAttribute(CssAttribute.BORDER_COLOR, "lightgray");
+        div.addAttribute(CssAttribute.BORDER_WIDTH, "1px");
+        div.addAttribute(CssAttribute.BORDER_STYLE, "solid");
+        div.addAttribute(CssAttribute.BORDER_RADIUS, "15px");
+        div.addAttribute(CssAttribute.MARGIN, "30px");
+        div.addAttribute(CssAttribute.WIDTH, "60%");
+        theme.add(div);
+
+        CssComponent img = new CssComponent(HtmlSelector.IMG.getSelector());
+        img.addAttribute(CssAttribute.WIDTH, "32px");
+        img.addAttribute(CssAttribute.HEIGHT, "32px");
+        theme.add(img);
+
+        CssComponent a = new CssComponent(HtmlSelector.A.getSelector());
+        a.addAttribute(CssAttribute.DISPLAY, "block");
+        a.addAttribute(CssAttribute.COLOR, "lightgray");
+        a.addAttribute(CssAttribute.MARGIN, "2% 5%");
+        a.addAttribute(CssAttribute.PADDING, "5%");
+        a.addAttribute(CssAttribute.TEXT_DECORATION, "none");
+        theme.add(a);
+
+        CssComponent button = new CssComponent(HtmlSelector.BUTTON.getSelector());
+        button.addAttribute(CssAttribute.COLOR, "lightgray");
+        button.addAttribute(CssAttribute.BORDER_COLOR, "lightgray");
+        button.addAttribute(CssAttribute.BORDER_WIDTH, "1px");
+        button.addAttribute(CssAttribute.BORDER_STYLE, "solid");
+        button.addAttribute(CssAttribute.BORDER_RADIUS, "15px");
+        button.addAttribute(CssAttribute.MARGIN, "1%");
+        button.addAttribute(CssAttribute.PADDING, "2%");
+        button.addAttribute(CssAttribute.BACKGROUND_COLOR, "black");
+        theme.add(button);
+
+        CssComponent copyButtonCss = new CssComponent(new Selector("."+copyButton+" "+HtmlSelector.BUTTON));
+        copyButtonCss.addAttribute(CssAttribute.BORDER, "0");
+        theme.add(copyButtonCss);
+
+        CssComponent aHover = new CssComponent(new Selector(HtmlSelector.A+":hover"));
+        aHover.addAttribute(CssAttribute.COLOR, "white");
+        aHover.addAttribute(CssAttribute.BORDER_COLOR, "white");
+        theme.add(aHover);
+
+        CssComponent buttonHover = new CssComponent(new Selector(HtmlSelector.BUTTON+":hover"));
+        buttonHover.addAttribute(CssAttribute.COLOR, "white");
+        buttonHover.addAttribute(CssAttribute.BORDER_COLOR, "white");
+        theme.add(buttonHover);
+
+        CssComponent inputHover = new CssComponent(new Selector(HtmlSelector.INPUT+":hover"));
+        inputHover.addAttribute(CssAttribute.COLOR, "white");
+        inputHover.addAttribute(CssAttribute.BORDER_COLOR, "white");
+        theme.add(inputHover);
+    }
+
+    @Override
+    public Douplet<Integer, Page> getResponse(RequestMethod requestMethod, String s, Map<String, String> map) {
+        Data.readData();
+
+        if (s.equals("/add")) {
+            Page addPage = new Page();
+            addPage.setTitle("Add coordinate");
+            addPage.addTheme(theme);
+            Form form = new Form("/", false);
+
+            form.add(new Label(NAME, "Name of the coordinate: "));
+            Input name = new Input("text");
+            name.setName(NAME);
+            form.add(name);
+            form.add(new StringHtmlComponent(StringModifiers.BR.toString()));
+
+            form.add(new Label(X, "X coordinate (integer) : "));
+            Input x = new Input("text");
+            x.setName(X);
+            form.add(x);
+            form.add(new StringHtmlComponent(StringModifiers.BR.toString()));
+
+            form.add(new Label(Y, "Y coordinate (integer) : "));
+            Input y = new Input("text");
+            y.setName(Y);
+            form.add(y);
+            form.add(new StringHtmlComponent(StringModifiers.BR.toString()));
+
+            form.add(new Label(Z, "Z coordinate (integer) : "));
+            Input z = new Input("text");
+            z.setName(Z);
+            form.add(z);
+            form.add(new StringHtmlComponent(StringModifiers.BR.toString()));
+
+            form.add(new Label(DIMENSION, "Dimension: "));
+            Select selectDim = new Select(DIMENSION);
+            for (Data.Dimension dim : Data.Dimension.values()) {
+                selectDim.add(new Option(dim.name(), dim.name()));
+            }
+            form.add(selectDim);
+            form.add(new StringHtmlComponent(StringModifiers.BR.toString()));
+
+            Button okButton = new Button("OK");
+            okButton.setSubmit();
+            form.add(okButton);
+
+            addPage.addComponent(form);
+            addPage.addComponent(new OneButtonForm("/", "Cancel", false));
+            return new Douplet<>(200, addPage);
+        }
+
+        if (map.get(NAME) != null) {
+            try {
+                Data.INSTANCE.addCoordinate(map.get(NAME), Integer.parseInt(map.get(X)),
+                        Integer.parseInt(map.get(Y)), Integer.parseInt(map.get(Z)),
+                        Data.Dimension.valueOf(map.get(DIMENSION)));
+                Data.writeData();
+                Page redirect = new Page();
+                redirect.addComponent(new Redirect("/", redirect));
+                return new Douplet<>(200, redirect);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        Page page = new Page();
+        page.setTitle("Coordinates");
+        page.addTheme(theme);
+
+        Table table = new Table();
+        table.addHead("Name");
+        table.addHead("Dimension");
+        table.addHead("X");
+        table.addHead("Z");
+
+        List<Data.Coordinate> coordinates = new ArrayList<>(Data.INSTANCE.getCoordinates());
+        coordinates.sort(new Data.CoordinateComparator());
+        for (Data.Coordinate coordinate : coordinates) {
+            table.add(coordinate.getName());
+            table.add("" + coordinate.getDimension());
+            table.add("" + (coordinate.getX() / 50) * 50);
+            table.add("" + (coordinate.getZ() / 50) * 50);
+            table.add(new A(Data.INSTANCE.getMapPrefix() + getMapCoordinates(coordinate), new Img("/files/map.png")));
+            table.add(new CopyButton(new Img("/files/copy.png"), page, getTpCommand(coordinate)).addClass(copyButton));
+            table.newRow();
+        }
+
+        page.addComponent(table);
+
+        page.addComponent(new FixedButton("/add", "Add new", new ButtonPosition("20px", "20px"), false));
+
+        return new Douplet<>(200, page);
+    }
+
+    private String getTpCommand(Data.Coordinate coordinate) {
+        return "tp @p " + coordinate.getX() + " " + coordinate.getY() + " " + coordinate.getZ();
+    }
+
+    private String getMapCoordinates(Data.Coordinate coordinate) {
+        return "index.html#/" + coordinate.getX() + "/64/" + coordinate.getZ() + "/-2/" + coordinate.getDimension().getLink();
+    }
+}
